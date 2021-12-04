00812f084d0198a108db407df77f3e28b8d9c9c8
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-04 15:35:44 -0600

    express done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// imports 
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
+
+app.use(morgan('dev'))
 
 app.listen(3000)
 console.log('server on port 3000');
