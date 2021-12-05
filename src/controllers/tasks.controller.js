5cee802e51ed03f4af37fcaca0bbaeb24035a96e
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-05 16:41:45 -0600

    adding update func

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -71,7 +71,20 @@ const deleteTask = async (req, res) => {
 }
 
 const updateTask = async (req, res) => {
-    res.send('updating a task');
+
+    const {id} = req.params;
+    const {title, description} = req.body;
+
+    const response = await pool.query(
+        "UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *",
+        [title,description,id]
+    )
+
+    if(result.rows.length === 0)return res.status(404).json({
+        message:"task not found"
+    })
+
+    res.send(result.rows[0]);
 }
 
 module.exports = {
