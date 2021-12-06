6ba580ba81f063adceeaa3fdd8f00485725c4ad1
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-05 20:16:23 -0600

    adding the routes task from

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar"
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 
 const App = () => {
   return (
     <div>
-    
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TaskList/>} />
+        <Route path='/tasks/new' element={<TaskForm/>} />
+        {/* edit route */}
+      </Routes>
+     </BrowserRouter>
     </div>
   )
 }
+
+
 
 export default App
