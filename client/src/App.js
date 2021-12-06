a3c442aa43f5bcb19ce1f8da6e7927ad8ba88cdb
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-05 20:32:56 -0600

    adding ui container

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Components
 import Navbar from "./components/Navbar"
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList'
 
+// UI
+import { Container } from '@mui/material'
+
 const App = () => {
   return (
     <div>
-     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<TaskList/>} />
-        <Route path='/tasks/new' element={<TaskForm/>} />
-        {/* edit route */}
-      </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        {/* WE ARE USEING THE CONTAINER FOR THE UI DESING  ------------------------------------ */}
+        <Container>
+          {/* WE ARE SETTING THE ROUTES RIGHT HERE -----------------------------------------   */}
+          <Routes>
+            <Route path='/' element={<TaskList/>} />
+            <Route path='/tasks/new' element={<TaskForm/>} />
+            {/* edit route */}
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </div>
   )
 }
