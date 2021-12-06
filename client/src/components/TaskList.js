f43d53448ab98d8e319bd8e3dbdc322ba7d3d00e
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-06 17:27:04 -0600

    adding hook effect into task list file

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,9 +1,20 @@
-
+import { useEffect } from "react"
 
 const TaskList = () => {
-    return (
+    
+  const loadTasks = async() =>{
+    const response = await fetch(`http://localhost:4000/tasks`);
+    const data = await response.json()
+  }
+
+  useEffect(()=>{
+
+  }, [])
+
+  return (
         <>
-          <h1>Task List</h1>  
+          <h1>Task List</h1>
+
         </>
     )
 }
