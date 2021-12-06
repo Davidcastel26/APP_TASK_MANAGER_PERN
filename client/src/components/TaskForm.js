3b12fe4eeaa7ee77b786a2f8357e663cda166fb4
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-06 12:46:48 -0600

    labes style done

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,13 +1,48 @@
-import { Card, Grid, Typography } from '@mui/material';
+import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
 
 const TaskForm = () => {
     return (
         <Grid container direction='column' alignItems='center' justifyContent='center'>
             <Grid item xs={3}>
-                <Card>
-                    <Typography>
+                <Card
+                    sx={{mt:5}}
+                    style={{
+                        backgroundColor:'#1e272e',
+                        padding: '1rem'
+                    }}
+                >
+                    <Typography variant='5' textAlign='center' color='white'>
                         CREATE TASK HERE!   
                     </Typography>
+                    <CardContent>
+                        <form action="">
+                            <TextField 
+                                variant='filled'
+                                label='Write your title'
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+                                inputProps={{style:{color:"white"}}}
+                                InputLabelProps={{style:{color:"white"}}}
+                            />
+                            <TextField
+                                variant='filled'
+                                label='Write your description'
+                                multiline
+                                rows={4}
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+                                inputProps={{style:{color:"white"}}}
+                                InputLabelProps={{style:{color:"white"}}}
+                            />
+                            <Button variant='contained' color='primary' type='submit'>
+                                Save
+                            </Button>
+                        </form>
+                    </CardContent>
                 </Card>
             </Grid>
         </Grid>
