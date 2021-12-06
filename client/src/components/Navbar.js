3146aa1daba086e7c0036624d55b1b92bc440483
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-05 21:03:24 -0600

    using btn box appbar container typography toolbar UI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,40 @@
-import {Button} from '@mui/material'
+//REACT
+import { Link, useNavigate } from 'react-router-dom'
+
+// UI
+import {Button, 
+        Box, 
+        AppBar, 
+        Container, 
+        Typography,
+        Toolbar} from '@mui/material'
+
+
 const Navbar = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            navbar
-            
-        </div>
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar position='state' color='transparent'>
+                <Container>
+                    <Toolbar>
+                        <Typography sx={{ flexGrow: 1}}>
+                            <Link to='/'>
+                                PERN Stack
+                            </Link> 
+                        </Typography>
+                        <Button 
+                            variant='contained' 
+                            color='primary' 
+                            onClick={ () => navigate("/tasks/new")}
+                        >
+                            New Task
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
     )
 }
 
